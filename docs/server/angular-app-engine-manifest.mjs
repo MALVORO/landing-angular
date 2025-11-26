dc1458c4fde2c1fce90ce21bd6cb874aca5433d1
Malvoro <vortizc@est.ups.edu.ec>
2025-11-26 10:42:36 -0500

    rebuild docs for github pages

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '.',
+  basePath: '/landing-angular',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: './',
+  baseHref: '/landing-angular/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/landing-angular"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 694, hash: '929f5998fff10198c6c232043e3b8aa0c5aaad7829549cea0a1094f589be2c73', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1012, hash: 'fb4b7dcb192a8c4002f6c862898c31160bcad9987d659baba89bd36a3b7019e4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 6073, hash: '99a5aac8090d58d634e24c84bcfe676baa7d4b7cb4de0c61662eef4fc8c47f61', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 709, hash: '15a511c2794d855176425923a0c8c8731b33dc9d4c79c1cfce29e9143b61c26a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1027, hash: 'c47908cdad9d04c0e044577143d7a8713ad6353bb87883e0cbd15fd4e16e572e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 6088, hash: 'b74582f2c0ed468e4da6c47bcfc8acfc8126a4c83a70c74f82e417a024213f38', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-CVMPDVDB.css': {size: 117, hash: 'UJ1Ft6Lwi7Q', text: () => import('./assets-chunks/styles-CVMPDVDB_css.mjs').then(m => m.default)}
   },
 };
